344313746454789b183cdbeb42c4ca8a4b475ff5
sebastian.schmidt <bastman77@gmail.com>
2019-02-17 11:00:13 +0100

    initial

--- example-kotlin/src/main/kotlin/azure/tika/Function.kt
+++ example-kotlin/src/main/kotlin/azure/tika/Function.kt
@@ -1,7 +1,28 @@
 package azure.tika
 
 import com.microsoft.azure.serverless.functions.ExecutionContext
+import com.microsoft.azure.serverless.functions.HttpRequestMessage
+import com.microsoft.azure.serverless.functions.annotation.AuthorizationLevel
+import com.microsoft.azure.serverless.functions.annotation.HttpTrigger
 import java.time.Instant
+import java.util.*
+
+
+/*
+public static String HttpTriggerJavaMetadata(
+@HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+@BindingName("firstName") String queryValue1, @BindingName("lastName") String queryValue2
+)
+*/
+
+
+fun foo(
+        @HttpTrigger(name = "req", methods = ["GET", "POST"] , authLevel = AuthorizationLevel.ANONYMOUS)
+        req: HttpRequestMessage<Optional<String>>?
+):String {
+    //AuthorizationLevel.ANONYMOUS
+    return "foo() req=$req"
+}
 
 fun ping(): String = "Hello from ping at ${now()} ."
 fun ping2(context: ExecutionContext): String {
